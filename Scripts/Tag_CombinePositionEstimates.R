1053ae4f930968e9bc7edffc63188da45ca3a64c
Johanna Wren <johanna.wren@noaa.gov>
2025-03-06 11:46:59 -1000

    updated file extensions and added both most probable track sst and tag sst to the final output tag

--- Scripts/Tag_CombinePositionEstimates.R
+++ Scripts/Tag_CombinePositionEstimates.R
@@ -4,6 +4,7 @@ library(data.table)
 library(here)
 library(readxl)
 library(here)
+library(ggplot2)
 
 # Set working directory
 mainDir <- here()
@@ -36,52 +37,48 @@ head(dataDF)
 # Get rid of all the unnecessary data and save just the tag number, date, location, and temperature.  
 dataFinal <- dataDF %>% 
   select(tagID, year, month, day, mptlon, mptlat, mptsst, tagsst) %>% 
-  rename(lon=mptlon, lat=mptlat, sst=mptsst)
+  rename(tag.serial=tagID, lon=mptlon, lat=mptlat)
+head(dataFinal)
 
 
 # Replacing the id's to be consistent with Dan's tags
-# Get all tag names and ids
-myVec1 <- lapply(strsplit(myFiles, '_'), "[", 3, drop=F) %>% 
-  unlist()
-myVec2 <- lapply(strsplit(myFiles, '_'), "[", 2, drop=F) %>% 
-  unlist()
 # Replace NA's with the other name so no NAs
-myVec2[which(myVec2 == 'NA')] <- myVec1[which(myVec2 == 'NA')]
-length(myVec1)
-length(myVec2)
+idxVec2[which(idxVec2 == 'NA')] <- idxVec1[which(idxVec2 == 'NA')]
+length(idxVec1)
+length(idxVec2)
 
-# Replace all tag.serial numbers with the second TagID that's in the filename
-for (i in seq_along(myVec2)) {
-  idx <- which(myDF$tag.serial == myVec2[i])
-  myDF$tag.serial[idx] <- myVec1[i]
-}
-head(myDF)
+# # Replace all tag.serial numbers with the second TagID that's in the filename
+# for (i in seq_along(myVec2)) {
+#   idx <- which(myDF$tag.serial == myVec2[i])
+#   myDF$tag.serial[idx] <- myVec1[i]
+# }
+# head(myDF)
 
 
 #-------------------------------------------------------------------------------
 #  IATTC FILES
 #-------------------------------------------------------------------------------
 # Read in files
-danFiles <- list.files(pattern = '.xlsx')
+danFiles <- list.files(path='Output', pattern = '.xlsx', full.names = T)
 danData1 <- read_xlsx(danFiles[1], sheet = 2) %>% 
-  select(dataname, year, month, day, mptlon, mptlat, tagsst)
+  select(dataname, year, month, day, mptlon, mptlat, mptsst, tagsst)
 danData2 <- read_xlsx(danFiles[1], sheet = 4) %>% 
-  select(dataname, year, month, day, mptlon, mptlat, tagsst)
+  select(dataname, year, month, day, mptlon, mptlat, mptsst, tagsst)
 danData3 <- read_xlsx(danFiles[2]) %>% 
   select(dataname, year, month, day, mptlon, mptlat, mptsst) %>% 
-  rename(tagsst=mptsst)
-danData4 <- read.csv('tL98479a.csv') %>% 
-  select(dataname, year, month, day, mptlon, mptlat, tagsst)
+  mutate(tagsst=NA)
+danData4 <- read.csv('Output/tL98479a.csv') %>% 
+  select(dataname, year, month, day, mptlon, mptlat, mptsst, tagsst)
 
 # Combine all and rename columns
 danData <- bind_rows(list(danData1, danData2, danData3, danData4), .id = 'id') %>% 
-  rename(tag.serial=dataname, lon=mptlon, lat=mptlat, sst=tagsst)
-
+  rename(tag.serial=dataname, lon=mptlon, lat=mptlat)
+head(danData)
 
 #-------------------------------------------------------------------------------
 # COMBINE ALL DATA
 #-------------------------------------------------------------------------------
-allData <- myDF %>% 
+allData <- dataFinal %>% 
   bind_cols(id='5') %>% 
   bind_rows(danData)
 
@@ -108,6 +105,13 @@ ggplot() +
   borders(database = 'world2', fill='black') + 
   geom_path(data=allData, aes(lon, lat, group=tag.serial, color=tag.serial)) + 
   coord_quickmap(xlim=c(140,280),ylim=c(-30,40)) + 
+  theme_bw()
+
+# Plot daily points
+ggplot() +
+  borders(database = 'world2', fill='black') +
+  geom_point(data=allData, aes(lon, lat, group = year, color=factor(year)), alpha=0.5, size=1) +
+  coord_quickmap(xlim=c(140,280), ylim=c(-30,40)) + 
   theme_bw()
 
 # How many tags?
